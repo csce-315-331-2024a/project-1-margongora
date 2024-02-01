8dd8aac91ec3c39278a80c5f9b9df47b0efb0138
margongora <ngongora@tamu.edu>
2024-02-01 17:49:47 -0600

    Added links and a tooltip, as well as commented on the toggle function

--- src/assets/js/toggle.js
+++ src/assets/js/toggle.js
@@ -1,20 +1,24 @@
 function toggleStyle() {
+
+    // Grab stylesheet by id and reference to its href
     var sheet = document.getElementById("styles");
     var name = sheet.getAttribute("href");
 
+    // Swap the stylesheet
     if (name === "assets/css/mood1.css") {
         name = "assets/css/mood2.css";
     } else {
         name = "assets/css/mood1.css";
     }
 
+    // Set and store the stylesheet
     sheet.setAttribute("href", name);
-
     localStorage.setItem("styles", name);
 }
 
 window.onload = function() {
-    const name = localStorage.getItem("styles");
+    // Load theme from storage, or the default if one doesn't exist
+    const name = localStorage.getItem("styles") || "assets/css/mood1.css";
     const sheet = document.getElementById("styles");
     sheet.setAttribute("href", name);
 }
